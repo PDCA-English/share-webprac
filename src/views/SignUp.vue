1d56274d29713a0efbd363055e76700355515555
Takuya <r28.6sa@gmail.com>
2021-02-24 13:15:41 +0900

    change URL

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -32,7 +32,7 @@ export default {
   methods: {
     auth() {
       axios
-        .post("herokuのURL/api/register", {
+        .post("https://wizardly-kilby-c714eb.netlify.app/api/register", {
           name: this.name,
           profile: this.profile,
           email: this.email,
