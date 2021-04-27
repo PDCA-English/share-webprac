e62e31ddb3957e7635e460deb59e617918223c3e
Takuya <r28.6sa@gmail.com>
2021-04-27 21:35:56 +0900

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
-import SignUp from "../views/SignUp.vue";
-import Profile from "../views/Profile.vue";
-import Detail from "../views/Detail.vue";
+import Todo from "../views/Todo.vue";
 import store from "../store/index";
 
 Vue.use(VueRouter);
@@ -12,39 +8,9 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "login",
-    component: Login,
-  },
-  {
-    path: "/signup",
-    name: "signup",
-    component: SignUp,
-  },
-  {
-    path: "/home",
-    name: "Home",
-    component: Home,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: "/detail/:id",
-    name: "detail",
-    component: Detail,
-    meta: {
-      requiresAuth: true,
-    },
-    props: true,
-  },
-  {
-    path: "/profile",
-    name: "profile",
-    component: Profile,
-    meta: {
-      requiresAuth: true,
-    },
-  },
+    name: "todo",
+    component: Todo,
+  }
 ];
 
 const router = new VueRouter({
